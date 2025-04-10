802a8666006da4a8fcb9378b6d435f58e785a330
Chebawca21 <quba2000@o2.pl>
2025-04-10 21:59:51 +0200

    remove test

--- app/test_main.py
+++ app/test_main.py
@@ -69,27 +69,3 @@ def test_missing_city():
                                "miasto": "Wrocław",
                                "wojewodztwo": "dolnośląskie"
                                }
-
-def test_response():
-    client.post("/struct", params={'adr': 'Wiesława Frano ul. Kwiatowa 17/5 50-123 dolnośląskie'})
-    client.post("/struct", params={'adr': 'Paweł Bandura ul. Bolesława Chrobrego 145 42-500 Będzin'})
-    client.post("/struct", params={'adr': 'Iwona Bagińska ul. Gołębia 3 15-336 Białystok'})
-    client.post("/struct", params={'adr': 'Bernard Kątny ul. J. Hallera 11  07-412 Ostrołęka'})
-    client.post("/struct", params={'adr': 'Antonina Duczyńska ul. Podjazd 78 81-805 Sopot'})
-    client.post("/struct", params={'adr': 'Izydor Jagodziński ul. Kilińskiego Jana 133 85-670 Bydgoszcz'})
-    client.post("/struct", params={'adr': 'Judyta Cisło  ul. Moniuszki Stanisława 54 41-406 Mysłowice'})
-    client.post("/struct", params={'adr': 'Klara Sugalska ul. Zakręt 93 02-907'})
-    client.post("/struct", params={'adr': 'Bartosz Kaczmarski ul. Wiejska 17 44-338 Jastrzębie-zdrój'})
-    response = client.post("/struct", params={'adr': 'Stefan Żaczek ul. Morcinka Gustawa 143 40-124'})
-    id = response.json()["uuid"]
-    response = client.get(f"/result/{id}")
-    assert response.status_code == 200
-    assert response.json() == {"uuid": id,
-                               "status": "DONE",
-                               "imie_nazwisko": "Stefan Żaczek",
-                               "ulica": "ul. Morcinka Gustawa",
-                               "numer_domu": "143",
-                               "kod_pocztowy": "40-124",
-                               "miasto": "Katowice",
-                               "wojewodztwo": "śląskie"
-                               }
